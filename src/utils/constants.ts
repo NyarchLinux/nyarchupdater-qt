73bd7849ff38227c1d97aeb710527f183b732b8f
TheDogHusky <contact@classydev.fr>
2026-02-20 14:17:08 +0100

    refactor: rewrite in python and qml

--- src/utils/constants.ts
+++
@@ -1,5 +0,0 @@
-import { paths } from "./paths";
-import { join } from "node:path";
-
-export const FIRST_RUN_PATH = join(paths.data, "first-run");
-export const ASSETS_PATH = join(__dirname, "assets");
--- src/utils/paths.ts
+++
@@ -1,5 +0,0 @@
-import envPaths from "env-paths";
-
-export const paths = envPaths("Nyarch Updater", {
-    suffix: "", // no need for suffix, there will only be one nyarch updater (see https://github.com/sindresorhus/env-paths)
-});
